d0f247999d45bd1554813073f7364ceb928e5955
Aniruddh <theaniruddhkadam@gmail.com>
2025-04-02 11:50:06 +0530

    updated dashboard

--- src/main/java/com/example/MyFirstApp/Repo/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.MyFirstApp.Repo;
-
-import com.example.MyFirstApp.Model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepo extends JpaRepository<Product, Long> {
-}
