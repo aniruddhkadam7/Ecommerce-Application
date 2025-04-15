8e35f4ba1ff74a5289d90dc270cfc921ce332138
Aniruddh <theaniruddhkadam@gmail.com>
2025-04-15 10:02:17 +0530

    update

--- src/main/java/com/example/hypercart/HypercartApplication.java
+++ src/main/java/com/example/hypercart/HypercartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication                 // Starting point of the Application Start the app to check
+@SpringBootApplication
 public class HypercartApplication {
 
 	public static void main(String[] args) {
