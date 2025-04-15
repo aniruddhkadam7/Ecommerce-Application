12a7247b3e9d142e1e225572cd32487c8406ceb3
Aniruddh <theaniruddhkadam@gmail.com>
2025-04-15 10:03:02 +0530

    update

--- src/main/java/com/example/hypercart/HypercartApplication.java
+++ src/main/java/com/example/hypercart/HypercartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication  // Lets go
 public class HypercartApplication {
 
 	public static void main(String[] args) {
