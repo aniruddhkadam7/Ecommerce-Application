fa151f3ad2a15271a2184ca8530fc05c73114781
Aniruddh <theaniruddhkadam@gmail.com>
2025-04-22 15:47:05 +0530

    implemented redis

--- src/main/java/com/example/hypercart/HypercartApplication.java
+++ src/main/java/com/example/hypercart/HypercartApplication.java
@@ -14,7 +14,7 @@ public class HypercartApplication {
 		SpringApplication.run(HypercartApplication.class, args);
 	}
 
-	@Bean         // its a beanddddssssssss
+	@Bean         // its a beanddddssssssssssss
 	CommandLineRunner init(RoleRepo roleRepo) {
 		return args -> {
 			if (roleRepo.findByName("USER") == null) {
