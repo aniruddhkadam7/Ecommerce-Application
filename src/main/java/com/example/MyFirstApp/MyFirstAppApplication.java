12936366686f10b8b2ac30efbaa93a2ec7623775
Aniruddh <theaniruddhkadam@gmail.com>
2025-04-26 11:09:17 +0530

    update

--- src/main/java/com/example/MyFirstApp/MyFirstAppApplication.java
+++ src/main/java/com/example/MyFirstApp/MyFirstAppApplication.java
@@ -8,7 +8,7 @@ public class MyFirstAppApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(MyFirstAppApplication.class, args); //hh
+		SpringApplication.run(MyFirstAppApplication.class, args); //hhlkk
 	}
 
 }
